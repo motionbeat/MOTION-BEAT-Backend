f5edc800e6e0c2be44b194e4da68bf28f7c6c887
jaenam <jaenam2060@gmail.com>
2024-05-02 10:55:45 +0900

    [feat] closing tab properly disconnects

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -79,6 +79,9 @@ const roomController = {
                     return currentRoom;
                 }
             }
+            if (req.body.live){
+                return currentRoom;
+            }
             res.status(200).json({message: "redirect"});
         } catch(err){
             res.status(500).json({message: "Internal Server Error"});            
