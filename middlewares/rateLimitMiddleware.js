e19b583da4f7084d728c1a5971f2688e46810e80
jaenam <jaenam2060@gmail.com>
2024-05-06 17:13:11 +0900

    [fix] rate limits for securty

---
+++ middlewares/rateLimitMiddleware.js
@@ -0,0 +1,15 @@
+import rateLimit from "express-rate-limit";
+
+// rate limit 설정
+const rateLimitOption = {
+  windowMs: 60 * 1000, // 1 minute
+  max: 100, // maximum 100 requests per windowMs
+  message: "Too many requests, please try again later.",
+};
+
+// Rate limiting middleware function
+const createRateLimiter = (options = rateLimitOption) => {
+  return rateLimit(options);
+};
+
+export default createRateLimiter;
--- routes/instrumentRouter.js
+++ routes/instrumentRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import instrumentController from "../controllers/instrumentController.js"; 
+import createRateLimiter from "../middlewares/rateLimitMiddleware.js";
 
+const limiter = createRateLimiter();
 const instrumentRouter = Router();
 
-instrumentRouter.get("/", instrumentController.showInstruments);
+instrumentRouter.get("/", limiter, instrumentController.showInstruments);
 
-instrumentRouter.patch("/select", instrumentController.selectInstrument);
+instrumentRouter.patch("/select", limiter, instrumentController.selectInstrument);
 
 export default instrumentRouter;
