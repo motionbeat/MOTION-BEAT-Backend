5c70da8c0cb35f6d10fb177d01c0686550fc9015
jaenam <jaenam2060@gmail.com>
2024-05-08 00:08:42 +0900

    [fix] rate limit options

--- middlewares/rateLimitMiddleware.js
+++ middlewares/rateLimitMiddleware.js
@@ -1,27 +1,18 @@
 import rateLimit from "express-rate-limit";
 
-// rate limit 설정
-const rateLimitOption = {
-  windowMs: 60 * 1000, // 1 minute
-  max: 100, // maximum 100 requests per windowMs
-  message: "Too many requests, please try again later.",
-  handler(req, res, next) {
-    req.headers['x-real-ip'] = req.ip;
-    next();
-  }
-};
-
 // Rate limiting middleware function
 const createRateLimiter = (options = {}) => {
-  // Trust only specific proxies and use the client's IP address from X-Forwarded-For header
   const trustProxy = options.trustProxy || ['loopback', 'linklocal', 'uniquelocal'];
 
   const rateLimitOptions = {
-    ...rateLimitOption,
+    windowMs: 60 * 1000, // 1 minute
+    max: 100, // maximum 100 requests per windowMs
+    message: "Too many requests, please try again later.",
     trustProxy,
     ...options
   };
 
   return rateLimit(rateLimitOptions);
 };
+
 export default createRateLimiter;
