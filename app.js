8a9afe33e6117570e0c7f2ccf9a3d1a3c1dd596b
jaenam <jaenam2060@gmail.com>
2024-05-15 20:04:51 +0900

    [fix] cb fix

--- app.js
+++ app.js
@@ -7,10 +7,25 @@ import createRateLimiter from "./middlewares/rateLimitMiddleware.js";
 const limiter = createRateLimiter();
 const app =  express();
 
+// app.use(cors({
+//     origin: ['http://192.168.0.0:3000', 'http://172.21.176.1:3000', 'http://localhost:3000'],
+//     credentials: true
+// }));
 app.use(cors({
-    origin: ['http://192.168.0.0:3000', 'http://172.21.176.1:3000', 'http://localhost:3000'],
+    origin: function (origin, callback) {
+        // Allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (origin.startsWith('http://192.168.') || origin === 'http://172.21.176.1:3000' || origin === 'http://localhost:3000') {
+            return callback(null, true);
+        }
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+    },
     credentials: true
 }));
+
 app.set('trust proxy', true);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
